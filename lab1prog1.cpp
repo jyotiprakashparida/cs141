d938d3d049e7551c5a5b4cc8f8aa51429254290f
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-08-30 15:26:13 +0530

    Create lab1prog1.cpp

---
+++ lab1prog1.cpp
@@ -0,0 +1,10 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {double a;
+    cout<<"enter the lenth in centimeters:";
+    cin>>a;
+    cout<<"the entered length in meters="<<a/100<<"m"<<endl;
+    	// your code goes here
+    	return 0;
+    }
